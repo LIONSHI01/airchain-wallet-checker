7c3de35ca4aed4ba83036414eccaf37de9a3263f
lionc <shizijian2010@yahoo.com.hk>
2024-07-18 00:08:59 +0800

    optimize tg alert

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ let walletResult: WalletResult[];
 bot.launch();
 
 async function init() {
-  wallets = loadLocalWalletList("./wallet-track-list");
+  wallets = loadLocalWalletList("./wallet-track-list.txt");
   walletResult = loadLocalWalletResult();
 }
 
@@ -31,56 +31,63 @@ async function main() {
   await init();
 
   let dataToWrite = "";
+  let tgErrorMessage = "";
+  try {
+    for (const wallet of wallets) {
+      let isEarningPoints = false;
+      let pointDiff = 0;
+      const { name, address } = wallet;
+      const walletDetails = await getWalletDetailsApi(address);
+      const walletPointsDetails = await getWalletPointsApi(address);
 
-  for (const wallet of wallets) {
-    let isEarningPoints = false;
-    let pointDiff = 0;
-    const { name, address } = wallet;
-    const walletDetails = await getWalletDetailsApi(address);
-    const walletPointsDetails = await getWalletPointsApi(address);
+      const { spendable_amount } = walletDetails.data || {};
+      const { total_points } = walletPointsDetails || {};
+      const isBalanceTooLow = Number(spendable_amount) < TOO_LOW_AMOUNT;
 
-    const { spendable_amount } = walletDetails.data || {};
-    const { total_points } = walletPointsDetails || {};
-    const isBalanceTooLow = Number(spendable_amount) < TOO_LOW_AMOUNT;
-
-    const walletResultRecord = walletResult.find((i) => i.address === address);
-    if (walletResultRecord) {
-      pointDiff = Number(total_points) - Number(walletResultRecord.points);
-      if (pointDiff > 0) {
-        isEarningPoints = true;
+      const walletResultRecord = walletResult.find(
+        (i) => i.address === address
+      );
+      if (walletResultRecord) {
+        pointDiff = Number(total_points) - Number(walletResultRecord.points);
+        if (pointDiff > 0) {
+          isEarningPoints = true;
+        }
       }
+
+      if (!isEarningPoints) {
+        const singleEarnPointWarningMsg = `🔴 ${name} is NOT earning points \n`;
+        tgErrorMessage += singleEarnPointWarningMsg;
+      }
+
+      if (isBalanceTooLow) {
+        const singleBalanceWarningMsg = `
+        🔴 Balance warning
+        Wallet: ${name}
+        Balance: ${spendable_amount}
+        \n`;
+        tgErrorMessage += singleBalanceWarningMsg;
+      }
+
+      // Write Result to Local File
+      const resultStringToFile = `${name},${address},${spendable_amount},${
+        total_points || walletResultRecord.points
+      }\n`;
+
+      dataToWrite += resultStringToFile;
+
+      // Log Data to Terminal
+      const signalEmoji = `${isBalanceTooLow ? "🔴" : "🟢"}`;
+      const resultLogString = `${signalEmoji} ${name} | $${spendable_amount} | Pts: ${
+        total_points || "Failed"
+      } | Growth:${isEarningPoints ? "⚡️" : "⚠️"} ${pointDiff}`;
+
+      logger.info(resultLogString);
     }
-
-    if (!isEarningPoints) {
-      const warningMsg = `🔴 ${name} is NOT earning points`;
-      bot.telegram.sendMessage(TELEGRAM_CHAT_ROOM_ID, warningMsg);
-    }
-
-    if (isBalanceTooLow) {
-      const warningMsg = `
-      🔴 Balance warning
-      Wallet: ${name}
-      Balance: ${spendable_amount}
-      `;
-      bot.telegram.sendMessage(TELEGRAM_CHAT_ROOM_ID, warningMsg);
-    }
-
-    // Write Result to Local File
-    const resultStringToFile = `${name},${address},${spendable_amount},${
-      total_points || walletResultRecord.points
-    }\n`;
-
-    dataToWrite += resultStringToFile;
-
-    // Log Data to Terminal
-    const signalEmoji = `${isBalanceTooLow ? "🔴" : "🟢"}`;
-    const resultLogString = `${signalEmoji} ${name} | $${spendable_amount} | Pts: ${
-      total_points || "Failed"
-    } | Growth:${isEarningPoints ? "⚡️" : "⚠️"} ${pointDiff}`;
-
-    logger.info(resultLogString);
+  } catch (error) {
+    console.log(error);
   }
 
+  bot.telegram.sendMessage(TELEGRAM_CHAT_ROOM_ID, tgErrorMessage);
   writeDataToLocal(filepath, dataToWrite);
 }
 
